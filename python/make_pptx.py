56b680931b7420f62af3fef3e2aa6c3c395e559a
Kojiro Yamamoto <h.wadsworth02@gmail.com>
2021-01-25 07:56:14 +0900

    Update 画像挿入の関数を作成しました

--- python/make_pptx.py
+++ python/make_pptx.py
@@ -5,42 +5,31 @@ from PIL import Image
 
 IMG_PATH = "../data/in/スマートグラス.jpg"
 IMG_PATH2 = "../data/in/ヨガ２.jpg"
-IMG_DISPLAY_HEIGHT = Inches(3) #スライドに表示するときの画像の高さ。とりあえず3インチとしておく。
-# SLIDE_OUTPUT_PATH = "test.pptx" #スライドの出力先パス
-
-#スライドオブジェクトの定義
-prs = Presentation() 
-
-TITLE_SLIDE = 0
-FREE_SLIDE = 1
-BRANK_SLIDE = 6
 
 
-def add_slide_title(prs, lay_out, text):
-    slide_layout = prs.slide_layouts[lay_out]
-    slide = prs.slides.add_slide(slide_layout)
+TITLE_SLIDE = prs.slide_layouts[0]
+FREE_SLIDE = prs.slide_layouts[1]
+BRANK_SLIDE = prs.slide_layouts[6]
+
+
+def add_title(slide, text):
     title = slide.shapes.title
     title.text = text
 
-
-add_slide_title(prs, TITLE_SLIDE, 'My slide' )
-add_slide_title(prs, FREE_SLIDE, 'これは２枚目' )
-# prs.save('../data/out/test.pptx')
-
-#スライドサイズを取得
-#
-prs = Presentation() 
-
-def add_image(slide, IMG_PATH, left_margin = 0, top_margin = 0.5, padding = 0.05):
+def add_image(slide, IMG_PATH, top = 0.1, right = 0, bottom = 0, left = 0, padding = 0.05):
     # スライドサイズ取得
-    SLIDE_WIDTH = prs.slide_width 
     SLIDE_HEIGHT = prs.slide_height
+    SLIDE_WIDTH = prs.slide_width 
 
     # スライドの画像を挿入する箇所のサイズを取得
-    top_margin = Inches(top_margin)
-    left_margin = Inches(left_margin)
-    slide_diplay_heigth = SLIDE_HEIGHT-top_margin
-    slide_diplay_width = SLIDE_WIDTH-left_margin
+    margin_top = SLIDE_HEIGHT * top
+    margin_bottom = SLIDE_HEIGHT * bottom
+    margin_left = SLIDE_WIDTH * left
+    margin_right = SLIDE_WIDTH * right
+
+    # 余白を除いた挿入サイズを取得
+    slide_diplay_heigth = SLIDE_HEIGHT - margin_top - margin_bottom 
+    slide_diplay_width = SLIDE_WIDTH - margin_left - margin_right
 
     # スライドの縦横比を計算
     slide_aspect_ratio = slide_diplay_width/slide_diplay_heigth
@@ -64,24 +53,40 @@ def add_image(slide, IMG_PATH, left_margin = 0, top_margin = 0.5, padding = 0.05
         # 縦横比を使って、画像の横を計算
         img_display_width = aspect_ratio*img_display_height
     #センタリングする場合の画像の左上座標を計算
-    left =((slide_diplay_width - img_display_width ) / 2) + left_margin
-    top = ((slide_diplay_heigth - img_display_height ) / 2) + top_margin
+    left =((slide_diplay_width - img_display_width ) / 2) + margin_left
+    top = ((slide_diplay_heigth - img_display_height ) / 2) + margin_top
     #画像をスライドに追加
     slide.shapes.add_picture(IMG_PATH, left, top, height = img_display_height)
 
-
+#スライドオブジェクトの定義
 prs = Presentation() 
-#白紙のスライドを追加
-blank_slide_layout = prs.slide_layouts[6] 
-slide = prs.slides.add_slide(blank_slide_layout)
+
+slide = prs.slides.add_slide(TITLE_SLIDE)
+add_title(slide, 'My slide' )
+
+slide = prs.slides.add_slide(FREE_SLIDE)
+add_title(slide, 'これは２枚目' )
+
+#スライドサイズを取得
+
 IMG_PATH = "../data/in/スマートグラス.jpg"
-add_image(slide, IMG_PATH)
-
-
-blank_slide_layout = prs.slide_layouts[6] 
-slide = prs.slides.add_slide(blank_slide_layout)
+add_image(slide, IMG_PATH, right = 0.5)
 IMG_PATH2 = "../data/in/ヨガ２.jpg"
-add_image(slide, IMG_PATH2)
+add_image(slide, IMG_PATH2,  left = 0.5)
+
+#白紙のスライドを追加
+slide = prs.slides.add_slide(BRANK_SLIDE)
+add_image(slide, IMG_PATH, right = 0.6, bottom = 0.3)
+add_image(slide, IMG_PATH2, right = 0.5, top = 0.6)
+add_image(slide, IMG_PATH, left = 0.5,  bottom = 0.3)
+add_image(slide, IMG_PATH2, left = 0.6, top = 0.6)
+
+#白紙のスライドを追加
+slide = prs.slides.add_slide(BRANK_SLIDE)
+
+left = top = width = height = Inches(1)
+txBox = slide.shapes.add_textbox(left, top, width, height)
+tf = txBox.text_frame
 
 #スライドを出力
 prs.save('../data/out/test2.pptx')
